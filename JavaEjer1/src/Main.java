57a552078348c9478094a162c654d2a48a6aac0b
Daniel González Muñoz <117729005+DaniPRK@users.noreply.github.com>
2022-12-02 09:48:52 +0100

    Update Main.java

--- JavaEjer1/src/Main.java
+++ JavaEjer1/src/Main.java
@@ -1,36 +1,36 @@
-import com.openbootcamp.Juego1;
-import com.openbootcamp.Juego2;
-import com.openbootcamp.VideoJuego;
+
 
 public class Main {
 
     public static void main(String[] args) {
 
-        VideoJuego Juego1 = new Juego1();
 
-          Juego1.setNombre("Minecraft");
-          Juego1.setMetascore(9.9);
-          Juego1.setMotor("java");
-          Juego1.setPrecio(19.8);
+        // enteros
+        byte number1 = 1; // 1 byte
+        short number2 = 10; // 2 bytes
+        int number3 = 50; // 4 bytes
+        long number4 = 100; // 8 bytes
 
-            System.out.println(Juego1.getNombre() + ", con motor basado en " + Juego1.getMotor() + ", precio de " + Juego1.getPrecio() + " y una puntuación de " + Juego1.getMetascore());
+        System.out.println("Numeros enteros: " + number1 + " , " + number2 + " , " + number3 + " , " + number4);
 
+        // decimales
+        float decimal1 = 4.9f;
+        double decimal2 = 9.99d;
 
-        VideoJuego Juego2 = new Juego2();
+        System.out.println("Decimales: " + decimal1 + " , " + decimal2);
 
+        // cadena de texto
+        String nombre = "Jose David";
+        String apellido = "Prieto";
 
-          Juego2.setNombre("FarCry");
-          Juego2.setMetascore(7.8);
-          Juego2.setMotor("Dunina Engine");
-          Juego2.setPrecio(49.99);
+        // caracter
+        char caracter1 = 'a';
 
+        System.out.println("Cadena de texto: " + nombre + " " + apellido + "\nCaracter: " + caracter1);
 
-            System.out.println(Juego2.getNombre() + ", con motor " + Juego2.getMotor() + ", precio de " + Juego2.getPrecio() + " y una puntuación de " +  Juego2.getMetascore() );
-
-
-
-
+        //booleanos
+        boolean verdadero = true;
+        boolean falso = false;
     }
 
-
-    }
+}
